572065a5c1a7421cfa5260217766de82b76b5ab5
AnuchitO <anuchitop@thoughtworks.com>
2021-03-06 15:24:25 +0700

    add middleware

--- server.go
+++ server.go
@@ -5,8 +5,27 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
 )
+
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID: 1, Title: "pay phone bills", Status: "active"},
+}
+
+func getTodosHandler(c echo.Context) error {
+	items := []*Todo{}
+	for _, item := range todos {
+		items = append(items, item)
+	}
+	return c.JSON(http.StatusOK, items)
+}
 
 func helloHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
@@ -16,7 +35,12 @@ func helloHandler(c echo.Context) error {
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/hello", helloHandler)
+	e.GET("/todos", getTodosHandler)
 
 	port := os.Getenv("PORT")
 	log.Println("port:", port)
