e83558468fd069d38c07251866d067a0aaa47ca3
AnuchitO <anuchitop@thoughtworks.com>
2021-03-13 11:50:10 +0700

    extract package

--- server.go
+++ server.go
@@ -5,29 +5,11 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/anuchito/jaygoapi/todo"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
-type Todo struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Status string `json:"status"`
-}
-
-var todos = map[int]*Todo{
-	1: &Todo{ID: 1, Title: "pay phone bills", Status: "active"},
-}
-
-func getTodosHandler(c echo.Context) error {
-	items := []*Todo{}
-	for _, item := range todos {
-		items = append(items, item)
-	}
-
-	items = append(items, &Todo{Title: os.Getenv("DATABASE_URL")})
-	return c.JSON(http.StatusOK, items)
-}
 
 func helloHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
@@ -42,7 +24,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/hello", helloHandler)
-	e.GET("/todos", getTodosHandler)
+	e.GET("/todos", todo.GetTodosHandler)
 
 	port := os.Getenv("PORT")
 	log.Println("port:", port)
